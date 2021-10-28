4fdc8ba1c18b5288efdcc7c03cecf9a1df76df03
Istarengwa <muller.lilian@hotmail.fr>
2021-10-28 11:10:51 +0200

    add finish migrate

--- db/migrate/20211028090230_create_strolls.rb
+++ db/migrate/20211028090230_create_strolls.rb
@@ -1,7 +1,9 @@
 class CreateStrolls < ActiveRecord::Migration[5.2]
   def change
     create_table :strolls do |t|
-
+      t.datetime :date
+      t.belongs_to :dogsitters, index: true
+      
       t.timestamps
     end
   end
